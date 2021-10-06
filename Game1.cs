8ce3a1e0e8299ad03f55152a5e82c6f8c4e63de7
Rehan164 <rehandsam@gmail.com>
2021-10-05 20:34:36 -0400

    Movement and Animation

--- Game1.cs
+++ Game1.cs
@@ -14,6 +14,8 @@ namespace Esteria
         Texture2D charDirection;
 
         Vector2 charPos = new Vector2(600,600);
+        Vector2 velocity;
+        bool hasJumped;
 
 
         //A Timer that stores milliseconds
@@ -25,19 +27,21 @@ namespace Esteria
         Rectangle[] idleRectanglesLeft;
         Rectangle[] runningRightRectangles;
         Rectangle[] runningLeftRectangles;
+        Rectangle[] jumpingRight;
+        Rectangle[] jumpingLeft;
 
         Rectangle[] charState;
 
         //These bytes tell the spriteBatch.Draw() what sourceRectangle to display
-        byte previousAnimationIndex;
         byte currentAnimationIndex;
+
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            _graphics.IsFullScreen = true;
+            _graphics.IsFullScreen = false;
          
         }
 
@@ -47,6 +51,7 @@ namespace Esteria
             _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
             _graphics.ApplyChanges();
             base.Initialize();
+            hasJumped = true;
         }
 
         protected override void LoadContent()
@@ -81,21 +86,46 @@ namespace Esteria
             runningRightRectangles[5] = new Rectangle(900, 111, 150, 111);
 
             runningLeftRectangles = new Rectangle[6];
-            runningLeftRectangles[0] = new Rectangle(150, 111, 150, 111);
-            runningLeftRectangles[1] = new Rectangle(300, 111, 150, 111);
-            runningLeftRectangles[2] = new Rectangle(450, 111, 150, 111);
-            runningLeftRectangles[3] = new Rectangle(600, 111, 150, 111);
-            runningLeftRectangles[4] = new Rectangle(750, 111, 150, 111);
-            runningLeftRectangles[5] = new Rectangle(900, 111, 150, 111);
+            runningLeftRectangles[0] = new Rectangle(0, 111, 150, 111);
+            runningLeftRectangles[1] = new Rectangle(150, 111, 150, 111);
+            runningLeftRectangles[2] = new Rectangle(300, 111, 150, 111);
+            runningLeftRectangles[3] = new Rectangle(450, 111, 150, 111);
+            runningLeftRectangles[4] = new Rectangle(600, 111, 150, 111);
+            runningLeftRectangles[5] = new Rectangle(750, 111, 150, 111);
+
+            jumpingRight = new Rectangle[10];
+            jumpingRight[0] = new Rectangle(0, 222, 150, 111);
+            jumpingRight[1] = new Rectangle(150, 222, 150, 111);
+            jumpingRight[2] = new Rectangle(300, 222, 150, 111);
+            jumpingRight[3] = new Rectangle(450, 222, 150, 111);
+            jumpingRight[4] = new Rectangle(600, 222, 150, 111);
+            jumpingRight[5] = new Rectangle(750, 222, 150, 111);
+            jumpingRight[6] = new Rectangle(900, 222, 150, 111);
+            jumpingRight[7] = new Rectangle(0, 333, 150, 111);
+            jumpingRight[8] = new Rectangle(150, 333, 150, 111);
+            jumpingRight[9] = new Rectangle(300, 333, 150, 111);
+
+            jumpingLeft = new Rectangle[10];
+            jumpingLeft[0] = new Rectangle(900, 222, 150, 111);
+            jumpingLeft[1] = new Rectangle(750, 222, 150, 111);
+            jumpingLeft[2] = new Rectangle(600, 222, 150, 111);
+            jumpingLeft[3] = new Rectangle(450, 222, 150, 111);
+            jumpingLeft[4] = new Rectangle(300, 222, 150, 111);
+            jumpingLeft[5] = new Rectangle(150, 222, 150, 111);
+            jumpingLeft[6] = new Rectangle(0, 222, 150, 111);
+            jumpingLeft[7] = new Rectangle(900, 333, 150, 111);
+            jumpingLeft[8] = new Rectangle(750, 333, 150, 111);
+            jumpingLeft[9] = new Rectangle(600, 333, 150, 111);
+
 
             // This tells the animation to start on the left-side sprite.
-            previousAnimationIndex = 1;
             currentAnimationIndex = 0;
 
         }
 
         protected override void Update(GameTime gameTime)
         {
+            charPos.Y += velocity.Y;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -123,17 +153,66 @@ namespace Esteria
                 charPos.X += 5;
                 charState = runningRightRectangles;
             }
-            if (!(Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.A)))
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) && hasJumped == false)
             {
+                
+                charPos.Y -= 10;
+                velocity.Y = -8;
+                hasJumped = true;
+            }
+
+            if (hasJumped == true)
+            {
+                float i = 1;
+                velocity.Y += 0.3f * i;
+
                 if (charDirection == adventureAnimationSheetRight)
                 {
-                    charState = idleRectanglesRight;
+                    charState = jumpingRight;
                 }
                 else if (charDirection == adventureAnimationSheetLeft)
                 {
-                    charState = idleRectanglesLeft;
+                    charState = jumpingLeft;
                 }
-               
+            }
+            if (charPos.Y >= 600)
+            {
+                hasJumped = false;
+            }
+            if (hasJumped == false)
+            {
+                velocity.Y = 0;
+            }
+
+
+
+
+
+            if (!(Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.A) || hasJumped == true))
+            {
+                if (charDirection == adventureAnimationSheetRight)
+                {
+                    if (charState != idleRectanglesRight)
+                    {
+                        currentAnimationIndex = 0;
+                    }
+                    charState = idleRectanglesRight;
+                    
+
+                }
+                else if (charDirection == adventureAnimationSheetLeft)
+                {
+
+                    if (charState != idleRectanglesLeft)
+                    {
+                        currentAnimationIndex = 0;
+                    }
+                    charState = idleRectanglesLeft;
+                    
+
+                }
+
             }
           
             
@@ -142,34 +221,53 @@ namespace Esteria
             // Check if the timer has exceeded the threshold.
             if (timer > threshold)
             {
-                // If player is in the middle sprite of the animation.
-                if (currentAnimationIndex == 1)
+
+                if (charState == jumpingRight || charState == jumpingLeft)
                 {
-                    // If the previous animation was the left-side sprite, then the next animation should be the right-side sprite.
-                    if (previousAnimationIndex == 0)
-                    {
-                        currentAnimationIndex = 2;
-                    }
-                    else
-                    // If not, then the next animation should be the left-side sprite.
+                    if (currentAnimationIndex >= jumpingRight.Length - 1)
                     {
                         currentAnimationIndex = 0;
                     }
-                    // Track the animation.
-                    previousAnimationIndex = currentAnimationIndex;
+                    else
+                    {
+                        currentAnimationIndex++;
+                    }
                 }
-                // If player was not in the middle sprite of the animation, he should return to the middle sprite.
-                else
+
+                else if (charState == idleRectanglesLeft || charState == idleRectanglesRight)
                 {
-                    currentAnimationIndex = 1;
+                    if (currentAnimationIndex >= idleRectanglesLeft.Length - 1)
+                    {
+                        currentAnimationIndex = 0;
+                    }
+                    else
+                    {
+                        currentAnimationIndex++;
+                    }
                 }
+
+               
+                else if (charState == runningLeftRectangles || charState == runningRightRectangles)
+                {
+                    if (currentAnimationIndex >= runningRightRectangles.Length - 1)
+                    {
+                        currentAnimationIndex = 0;
+                    }
+                    else
+                    {
+                        currentAnimationIndex++;
+                    }
+                }
+
+               
+
                 // Reset the timer.
                 timer = 0;
             }
             // If the timer has not reached the threshold, then add the milliseconds that have past since the last Update() to the timer.
             else
             {
-                timer += (float)gameTime.ElapsedGameTime.Milliseconds;
+                timer += (float)gameTime.ElapsedGameTime.Milliseconds + 30;
             }
 
             base.Update(gameTime);
